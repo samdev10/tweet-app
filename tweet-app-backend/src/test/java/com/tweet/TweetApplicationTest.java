a4fce22c3706a91cb09ab3df71e00d6112e1b267
samdev10 <santhoshn1099@outlook.com>
2019-11-16 11:18:22 -0500

    tweet-app

---
+++ tweet-app-backend/src/test/java/com/tweet/TweetApplicationTest.java
@@ -0,0 +1,12 @@
+package com.tweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
