02bdd3683f56910b9b1164b904efae34f2809256
samdev10 <santhoshn1099@outlook.com>
2020-02-15 12:17:17 -0500

    Fix build

---
+++ tweet-app-backend/src/main/java/com/tweet/security/InMemoryConfig.java
@@ -0,0 +1,32 @@
+package com.tweet.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * In Memory Config.
+ * @author santh
+ */
+public class InMemoryConfig extends WebSecurityConfigurerAdapter {
+    /*
+     * {@inheritDoc}
+     */
+    @Override
+    public void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+            .password(passwordEncoder().encode("pas"))
+            .roles("USER");
+    }
+
+    /**
+     * @return password encoder.
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
