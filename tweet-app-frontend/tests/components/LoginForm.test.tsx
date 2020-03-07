80989eb977371f87758377473d9b161119a1bde4
samdev10 <santhoshn1099@outlook.com>
2020-03-07 01:22:24 -0500

    Add more tests

--- tweet-app-frontend/tests/components/LoginForm.test.tsx
+++ tweet-app-frontend/tests/components/LoginForm.test.tsx
@@ -24,6 +24,31 @@ describe("<LoginForm />", () => {
     expect(wrapper.find(".error").text()).toBe("error");
   });
 
+  it("will render inputs", () => {
+    // When
+    const wrapper = shallow(<LoginForm handleSubmit={() => {}} error="" />);
+
+    // Then
+    expect(
+      wrapper
+        .find("input")
+        .at(0)
+        .prop("name")
+    ).toBe("username");
+    expect(
+      wrapper
+        .find("input")
+        .at(1)
+        .prop("name")
+    ).toBe("password");
+    expect(
+      wrapper
+        .find("input")
+        .at(2)
+        .prop("type")
+    ).toBe("submit");
+  });
+
   it("will call handle submit", () => {
     // Given
     let isCalled = false;
