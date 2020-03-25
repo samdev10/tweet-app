3fec82d9e3499b4532c5aed350950d1befaa1df0
samdev10 <santhoshn1099@outlook.com>
2020-03-25 01:50:34 -0400

    Create set up for selenium testing

---
+++ tweet-selenium/src/test/java/com/tweet/AbstractTestNG.java
@@ -0,0 +1,36 @@
+package com.tweet;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+
+import com.tweet.data.config.MongoConfig;
+import com.tweet.repository.UserInfoRepository;
+
+@ContextConfiguration(classes = { MongoConfig.class })
+@EnableMongoRepositories(basePackages = "com.tweet.repository")
+public class AbstractTestNG extends AbstractTestNGSpringContextTests {
+    private WebDriver driver;
+
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    @BeforeMethod
+    public void testSetup() throws IOException {
+        driver = new ChromeDriver();
+        String baseUrl = "http://localhost:8080";
+        driver.get(baseUrl);
+    }
+
+    @AfterTest
+    public void close() {
+        driver.close();
+    }
+}
---
+++ tweet-selenium/src/test/java/com/tweet/test/LoginTest.java
@@ -0,0 +1,14 @@
+package com.tweet.test;
+
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+public class LoginTest extends AbstractTestNGSpringContextTests {
+    @Test
+    public void mustLoginIntoTweetApp() {
+        // Given
+        // When
+        // Then
+    }
+
+}
