360598986118a856c340c4426cdc18a00bc22166
samdev10 <santhoshn1099@outlook.com>
2020-04-02 00:49:05 -0400

    Add redis service

--- tweet-app-backend/src/main/java/com/tweet/TweetApplication.java
+++ tweet-app-backend/src/main/java/com/tweet/TweetApplication.java
@@ -1,15 +1,19 @@
 package com.tweet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.oembedler.moon.graphql.boot.GraphQLWebsocketAutoConfiguration;
+import com.tweet.configuration.RedisConfiguration;
 import com.tweet.data.config.MongoConfig;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.tweet")
-@Import({ MongoConfig.class })
+@Import({ MongoConfig.class, RedisConfiguration.class })
+@EnableAutoConfiguration(exclude = { GraphQLWebsocketAutoConfiguration.class })
 public class TweetApplication {
 
     public static void main(String[] args) {
