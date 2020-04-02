d202fa331f90a577c40da91fdaa8759396a90b13
samdev10 <santhoshn1099@outlook.com>
2020-04-02 11:19:04 -0400

    Remove deprecated class

--- tweet-app-login/src/main/java/com/tweet/data/config/MongoConfig.java
+++ tweet-app-login/src/main/java/com/tweet/data/config/MongoConfig.java
@@ -2,20 +2,21 @@ package com.tweet.data.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 /**
  * Mongo Config.
  */
 @Configuration
-public class MongoConfig extends AbstractMongoConfiguration {
+public class MongoConfig extends AbstractMongoClientConfiguration {
 
     @Value("${datasource.client:localhost}")
     private String client;
 
-    @Value("${datasource.database:tweet-app}")
+    @Value("${datasource.database:tweet-app-test}")
     private String databaseName;
 
     /*
